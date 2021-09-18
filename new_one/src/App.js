9f6dcc1dfb579d3fc8ffd946c8910932f8092514
Spencer Knight <spencer.knightusn@gmail.com>
2021-09-17 17:09:07 -0700

    testing

---
+++ new_one/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  const ONE = navigator.appVersion;
+  return (
+    <div className='App'>
+      <div>`${ONE}`</div>
+      <div>hi</div>
+    </div>
+  );
+}
+
+export default App;
