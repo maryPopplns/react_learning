9473db852faa7f77b2a005f0d7946847eabbc54d
Spencer Knight <spencer.knightusn@gmail.com>
2021-09-22 11:59:11 -0700

    save point

--- new_one/src/App.js
+++ new_one/src/App.js
@@ -6,19 +6,65 @@ class App extends Component {
     this.state = {
       topText: '',
       bottomText: '',
-      randomImage: '',
+      randomImage: 'http://i.imgflip.com/1bij.jpg',
+      allMemeImgs: '',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.clickHandler = this.clickHandler.bind(this);
   }
 
   async componentDidMount() {
-    const RESPONSE = await fetch('http://i.imgflip.com/1bij.jpg');
-    const JSON = await RESPONSE.json();
-    console.log(JSON);
+    const RESPONSE = await fetch('https://api.imgflip.com/get_memes');
+    const RESOLVED = await RESPONSE.json();
+    const MEMES = RESOLVED.data.memes;
+    this.setState({ allMemeImgs: MEMES });
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  clickHandler(event) {
+    event.preventDefault();
+    const RANDOM_NUMBER = Math.floor(Math.random() * 100);
+    const NEW_IMAGE = this.state.allMemeImgs[RANDOM_NUMBER];
+    this.setState({ randomImage: NEW_IMAGE });
   }
 
   render() {
-    return <h1>{this.state.randomImage}</h1>;
+    return (
+      <div>
+        <form className='meme-form'>
+          <input
+            name='topText'
+            type='text'
+            value={this.state.topText}
+            onChange={this.handleChange}
+          />
+          <input
+            name='bottomText'
+            type='text'
+            value={this.state.bottomText}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.clickHandler}>Gen</button>
+        </form>
+        <div className='meme'>
+          <img src={this.state.randomImage} alt='' />
+          <h2 className='top'>{this.state.topText}</h2>
+          <h2 className='bottom'>{this.state.bottomText}</h2>
+        </div>
+      </div>
+    );
   }
 }
+
+/**
+ * Create a method that, when the "Gen" button is clicked, chooses one of the
+ * memes from our `allMemeImgs` array at random and makes it so that is the
+ * meme image that shows up in the bottom portion of our meme generator site
+ */
 
 export default App;
