9387984db3a9ecc6c70218e52c03effbff2fe8bb
Spencer Knight <spencer.knightusn@gmail.com>
2021-09-23 08:38:57 -0700

    start of new exercise

--- new_one/src/App.js
+++ new_one/src/App.js
@@ -1,65 +1,6 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      topText: '',
-      bottomText: '',
-      randomImage: 'http://i.imgflip.com/1bij.jpg',
-      allMemeImgs: '',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.clickHandler = this.clickHandler.bind(this);
-  }
-
-  async componentDidMount() {
-    const RESPONSE = await fetch('https://api.imgflip.com/get_memes');
-    const RESOLVED = await RESPONSE.json();
-    const MEMES = RESOLVED.data.memes;
-    this.setState({ allMemeImgs: MEMES });
-  }
-
-  handleChange(event) {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  }
-
-  clickHandler(event) {
-    event.preventDefault();
-    const RANDOM_NUMBER = Math.floor(Math.random() * 100);
-    const NEW_IMAGE = this.state.allMemeImgs[RANDOM_NUMBER];
-    this.setState({ randomImage: NEW_IMAGE });
-  }
-
-  render() {
-    return (
-      <div>
-        <form className='meme-form'>
-          <input
-            name='topText'
-            type='text'
-            value={this.state.topText}
-            onChange={this.handleChange}
-          />
-          <input
-            name='bottomText'
-            type='text'
-            value={this.state.bottomText}
-            onChange={this.handleChange}
-          />
-          <button onClick={this.clickHandler}>Gen</button>
-        </form>
-        <div className='meme'>
-          <img src={this.state.randomImage} alt='' />
-          <h2 className='top'>{this.state.topText}</h2>
-          <h2 className='bottom'>{this.state.bottomText}</h2>
-        </div>
-      </div>
-    );
-  }
-}
+class App extends Component {}
 
 /**
  * Create a method that, when the "Gen" button is clicked, chooses one of the
